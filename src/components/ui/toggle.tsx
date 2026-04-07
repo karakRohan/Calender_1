f27e472e65dbe7ce331bc6b11b967a755025b98a
karakRohan <karakrohan2005@gmail.com>
2026-04-08 00:04:37 +0530

    add

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -25,6 +25,7 @@ const toggleVariants = cva(
   },
 );
 
+
 const Toggle = React.forwardRef<
   React.ElementRef<typeof TogglePrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
